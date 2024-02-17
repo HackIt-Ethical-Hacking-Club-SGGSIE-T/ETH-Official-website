f95b0d46bde9e82faad4b353230efd67d325e058
HarromPS <hackit@sggs.ac.in>
2024-02-17 18:03:14 +0530

    File changed

--- ethical-hacking-hackit-club/pages/index.js
+++ ethical-hacking-hackit-club/pages/index.js
@@ -5,6 +5,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  let a=10;
+  console.log(a)
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
@@ -12,6 +14,7 @@ export default function Home() {
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started to build&nbsp;
+          <Link href='/sample' className="cursor-pointer px-2">ETH SITE</Link>
           <code className="font-mono font-bold">ETH site</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
---
+++ ethical-hacking-hackit-club/pages/sample.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const sample = () => {
+  return (
+    <div className='w-full bg-red-400 text-2xl'>This is a sample</div>
+  )
+}
+
+export default sample
